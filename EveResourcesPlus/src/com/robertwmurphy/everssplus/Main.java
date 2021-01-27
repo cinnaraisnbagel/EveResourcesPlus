1cf124617f3f8ea9230dead0a6c63428031ae02d
rwm77 <rwm77@192.168.1.142>
2021-01-26 21:59:08 -0600

    second test

--- EveResourcesPlus/src/com/robertwmurphy/everssplus/Main.java
+++ EveResourcesPlus/src/com/robertwmurphy/everssplus/Main.java
@@ -14,7 +14,6 @@ public class Main extends Application {
         primaryStage.setTitle("Eve Resources+");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
-        System.out.println("This is a change");
     }
 
 
